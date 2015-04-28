98c978054542f1d0469ff583a1b3859fcd0cba46
Lisyk <sergey.liskovoy@gmail.com>
2015-04-28 17:51:58 -0400

    add tre_ex and fix some previous

--- exercises.rb
+++ exercises.rb
@@ -1,19 +1,13 @@
 @array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 def one_ex
-@array.each do |i|
-  puts i
-  end
+@array.each {|num| puts num}
 end
 
 def two_ex
-@array.each do |i|
-  puts "map method #{i}"
-  end
+  @array.each {|num| puts num if num > 5}
 end
 
 def tree_ex
-  @array.each do |i|
-    puts i if i>5
-  end
+  @array.select {|num| num%2==0  }
 end
